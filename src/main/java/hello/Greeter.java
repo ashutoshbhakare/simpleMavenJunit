a495e4b7f709385b1ae3fe3ec3000e15b0b66d08
ashutoshbhakare <bhakareashutosh@yahoo.com>
2024-12-17 13:05:12 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello WORLD welcome to UNNATI KUCL JENKINS !! Hi and Bye  hello is the string needed here. testig for CKACLASS for webhooks testing on OCT 2024";
+    return "Hello WORLD welcome to CI / CD CLass !! ";
   }
 }
